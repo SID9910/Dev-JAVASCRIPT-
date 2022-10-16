4b25b51aea5afbaa827323a9696a1464115ad9e0
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-10-16 12:13:02 +0530

    dev

---
+++ Arrays_CustomMap.js
@@ -0,0 +1,29 @@
+//is se hum aapna map bna sakte hai
+
+//custom map main hum aaona map banakar use kar sakte hai
+//aapna map create ho chuka hai
+//use karlo Mymap kaise bhi
+Array.prototype.myMap =function(callback){
+    let res =[];
+
+    for(let i=0;i<this.length;i++){
+      let val =this[i];
+      let rv =callback(val,i,this);
+      res.push(rv);
+    }
+    return res;
+}
+//normal map
+let arr =[2,3,5,3,2,11];
+let s =arr.map(function(v,i,arr){
+    return v*v;
+});
+console.log(s);
+
+
+//my map same work karega sab main 
+//my map kisi main bhi use kar sakte hai
+let s2 =arr.myMap(function(v,i,arr){
+    return v*v;
+});
+console.log(s2);
---
+++ Arrays_Map.js
@@ -0,0 +1,92 @@
+//map is itself a function
+//map return a new arrasy equal in length to original array
+//map takes as input a callback fn
+//the callback fn takes 3 parameter(v,i,arr)
+
+//map will call the callback multiple times (once for each value)
+//for each run of callback ,map will pass v and i to callback
+//single value returned by each run of callback will be collected in a new array
+//map returns that new array
+
+// let arr =[2,5,9,8,15,11,6];
+
+// let sarr =arr.map(function(v,i,arr ){
+
+//     return v*v;
+// });
+// console.log(sarr);
+
+//trick 
+
+// let arr =[
+//    "Summet Malik",
+//    "Amit malik",
+//    "Siddharth gupta",
+//    "Pratham Gupta",
+//    "Kabir Gupta"
+// ]
+// let res =arr.map(function(v,i,arr){
+//     let namespart =v.split(" ");
+//       let first =namespart[0];
+//       let surname =namespart[1];
+//       let ffchar =first[0];
+//       let secchar =surname[0];
+//       let chae =ffchar+"."+secchar+".";
+//       return chae;
+//     });
+//     console.log(res);
+
+
+//question
+
+let arr =[
+    {
+        gender:'M',
+        age:34
+
+    },
+    {
+        gender:'F',
+        age:30
+    },
+    {
+        gender:'F',
+        age:12
+    },
+    {
+        gender:'M',
+        age:31
+    },
+    {
+        gender:'F',
+        age:12
+    },
+    {
+        gender:'M',
+        age:32
+    },
+    {
+        gender:'F',
+        age:30
+    }
+]
+
+let shortlist =arr.map(function(v,i,arr){
+    if(v.gender =='F'  && v.age>=20 && v.age<=30){
+        return true;
+    }else {
+        return false;
+    }
+});
+// console.log(shortlist);
+
+//map can be use like this also
+let s12 =arr.map((v,i,arr) =>v.gender =='F'  && v.age>=20 && v.age<=30 );
+console.log(s12);
+
+//map can also be use like this
+
+let s2 =arr.map((v,i,arr)=>{
+    return  v.gender =='F'  && v.age>=20 && v.age<=30;
+});
+console.log(s2);
